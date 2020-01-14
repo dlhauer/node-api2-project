239e6574eb0c95b1eeebdd4ffb63c2f13722309b
Dan Hauer <dlhauer@Dans-MacBook-Pro.local>
2020-01-14 14:03:51 -0800

    POST methods

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -54,5 +54,57 @@ router.get('/:id/comments', (req, res) => {
     });
 });
 
+router.post('/', (req, res) => {
+  if( !req.body.title || !req.body.contents) {
+    res.status(400).json({
+      message: 'Whoa whoa whoa, you didn\'t think you could submit something without a title or contents, did you? Try again, hotshot.'
+    })
+  } else {
+    const postData = req.body;
+    Posts.insert(postData)
+      .then( id => {
+        res.status(201).json(id);
+      })
+      .catch( error => {
+        console.log(error);
+        res.status(500).json({
+          message: 'There was an error saving the post to the database.'
+        });
+      });
+  }
+});
+
+router.post('/:id/comments', (req, res) => {
+  const id = req.params.id;
+  if (!req.body.text) {
+    res.status(400).json({
+      message: 'Gotta have some text in that comment, bro.'
+    })
+  } else {
+    Posts.findById(id)
+      .then( post => {
+        if(post.length > 0) {
+          const comment = {
+            text: req.body.text,
+            post_id: req.params.id
+          };
+          Posts.insertComment(comment)
+            .then( id => {
+              res.status(201).json(id);
+            })
+            .catch( error => {
+              res.status(500).json({
+                message: 'Error saving comment to database.'
+              });
+            });
+        } else {
+          res.status(404).json({
+            message: 'The post with the specified ID doesn\'t exist.'
+          });
+        }
+    })
+  }
+})
+
 
 module.exports = router;
